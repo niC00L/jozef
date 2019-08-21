067f9f8d1c706591f84756e71e015516388518bb
niC00L <nika.blanarova@gmail.com>
2019-08-21 20:56:34 +0200

    Menus

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+    public GameObject menuPanel;
+    public GameObject inventoryPanel;
+    public GameObject onScreenPanel;
+
+    void Start()
+    {
+        onScreenPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
+        menuPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuPanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Pause()
+    {
+        inventoryPanel.SetActive(false);
+        onScreenPanel.SetActive(false);
+        menuPanel.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        onScreenPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
