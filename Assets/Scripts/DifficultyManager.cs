421b8b6c4c359fcae7c6f610cfc6833cf6635454
niC00L <nika.blanarova@gmail.com>
2020-06-17 11:40:59 +0200

    Main menu scene

---
+++ Assets/Scripts/DifficultyManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyManager : MonoBehaviour
+{
+    public static int Difficulty = 1;
+    public static bool adaptiveDifficulty = true;
+
+}
